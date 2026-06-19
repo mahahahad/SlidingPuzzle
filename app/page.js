439c30a754667fdc2432a88962f753d657f29c8a
mahahahad <mahad.abd2003@gmail.com>
2026-06-19 20:27:00 +0400

    feat: add timer stat

--- app/page.js
+++ app/page.js
@@ -3,18 +3,29 @@
 import styles from '../styles/Home.module.css';
 import Grid from "./grid"
 import StatWrapper from './statWrapper';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import OptionsWrapper from './optionsWrapper';
 
 export default function Home() {
   const [moves, setMoves] = useState(0);
+  const [time, setTime] = useState(0);
   const [size, setSize] = useState(3);
   const [shuffle, setShuffle] = useState(false);
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [])
 
   return (
     <div className={styles.container}>
       <div className={styles.gridWrapper}>
         <div className={styles.stats}>
+          <StatWrapper type={"time"} value={time} />
           <StatWrapper type={"moves"} value={moves} />
         </div>
         <div className={styles.gameplayArea}>
